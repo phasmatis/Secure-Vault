45d71a6b864d45c5166209e76f93f4f35828feba
Phasmatise <manderer94@gmail.com>
2024-01-19 10:17:57 -0600

    added workshop.js

---
+++ workshop.js
@@ -0,0 +1,20 @@
+//1. Make a string with the intro phrase
+const introMessage =
+  "You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
+console.log(introMessage);
+
+/*2. Make 3 variables
+   - 1st variable must equal 10
+   - 2nd var must equal 40
+   - 3rd var must equal 39
+   */
+const num1 = 2 * 5;
+const num2 = 8 + 8 * 4;
+const num3 = 289 % 50; // % modulo operator returns the "remainder"
+console.log("Number 1:", num1);
+console.log("Number 1:", num2);
+console.log("Number 1:", num3);
+//4. (comment code)
+
+//5. Create a dialog box with the values
+alert(introMessage + "\n" + num1 + " - " + num2 + " - " + num3);
